34f91620b1b32b710cbf07c64995564c6f38aab3
DieRednax <xt.delange@gmail.com>
2025-11-20 12:07:11 +0200

    Items

---
+++ src/main/java/com/redfox/helloworldmod/item/ModItems.java
@@ -0,0 +1,29 @@
+package com.redfox.helloworldmod.item;
+
+import com.redfox.helloworldmod.HelloWorldMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(HelloWorldMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        HelloWorldMod.LOGGER.info("Registering mod items for " + HelloWorldMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(PINK_GARNET);
+            entries.add(RAW_PINK_GARNET);
+        });
+    }
+}

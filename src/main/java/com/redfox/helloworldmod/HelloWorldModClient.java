36a72d55609c354b4f177ba352dfdecb2f9c8827
DieRednax <xt.delange@gmail.com>
2025-11-20 11:04:06 +0200

    Initial commit

---
+++ src/main/java/com/redfox/helloworldmod/HelloWorldModClient.java
@@ -0,0 +1,10 @@
+package com.redfox.helloworldmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HelloWorldModClient  implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
